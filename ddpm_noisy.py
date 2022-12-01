748b09e48a5696be3aac0d75dd61914b1ba0edc7
kulunud <kulunud@uchicago.edu>
2022-11-30 23:54:14 -0600

    Update ddpm_noisy.py

--- ddpm_noisy.py
+++ ddpm_noisy.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 from torch import optim
 from utils2 import *
-from modules import UNet
+from modules32 import UNet
 import logging
 from torch.utils.tensorboard import SummaryWriter
 
